4eb30b1a09a3f15eff62fdd893bb3e1515be7e38
massahoudou <massahoudouodanou@gmail.com>
2021-03-18 15:20:30 +0000

    htaccess

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -95,7 +95,7 @@ class AppCustomAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        //return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
 
     }
 
